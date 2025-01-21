031cb8c40c217cc549883c52cc5a69345d4a1cc1
1247004718 <31641368+1247004718@users.noreply.github.com>
2025-01-21 22:22:30 +0800

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -428,9 +428,9 @@ user openvpn
 group openvpn
 key-direction 1
 verb 3" > /etc/openvpn/server/client-common.txt
-if [[ "$protocol" = "udp" ]]; then
-	echo "explicit-exit-notify 1" >> /etc/openvpn/server/client-common.txt
-fi
+#if [[ "$protocol" = "udp" ]]; then
+#	echo "explicit-exit-notify 1" >> /etc/openvpn/server/client-common.txt
+#fi
 	# Enable and start the OpenVPN service
 	systemctl enable --now openvpn-server@server.service
 	# Generates the custom client.ovpn
